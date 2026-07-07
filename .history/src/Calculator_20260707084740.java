f0f980060358a0f4559866159dbcf856dc0f994c
Henrique <henrique_schorck@estudante.sesisenai.org.br>
2026-07-07 08:57:57 -0300

    9h

---
+++ .history/src/Calculator_20260707084740.java
@@ -0,0 +1,31 @@
+public class Calculator {
+    // Private Attributes
+    private double num1;
+    private double num2;
+    // Constructor
+    public Calculator(double num1, double num2) {
+    this.num1 = num1;
+    this.num2 = num2;
+    }
+    // Getter and Setter Methods
+    public double getNum1() { return num1; }
+    public void setNum1(double num1) { this.num1 = num1; }
+
+    public double getnum2() { return num2; }
+    public void setnum2(double num2) { this.num2 = num2; }
+
+    // Operations 
+    public double sum() {
+    return this.num1 + this.num2;
+    }
+    public double divide() {
+    return this.num1 / this.num2;
+    }
+    public double subtract() {
+    return this.num1 - this.num2;
+    }
+    public double multiply() {
+    return this.num1 * this.num2;
+    }
+}
+
---
+++ .history/src/Main_20260707083357.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("=================================");
+        System.out.println("SISTEMA APRENDA & CALCULE");
+        System.out.println("=================================");
+        System.out.println("1. Menu Calculadora");
+        System.out.println("2. Guia de Conceitos");
+        System.out.println("3. Sair do Sistema");
+        System.out.println("=================================");
+    }
+}
